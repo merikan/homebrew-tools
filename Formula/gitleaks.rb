71c4fd11b64f0006340566aed2ed3ec790735a53
Peter Merikan <peter@merikan.com>
2018-11-12 21:07:02 +0100

    Update gitleaks.rb

--- Formula/gitleaks.rb
+++ Formula/gitleaks.rb
@@ -5,6 +5,16 @@ class Gitleaks < Formula
   version "1.19.3"
   sha256 "84396cef693dc064869789a6b56087085639d46beec3025a33bd626533e7534e"
 
+  def caveats; <<~EOS
+      This is not the official Formula from the creator of `gitleaks`. It is only for my personal 
+      use until Zachary provides a official Formula.
+      In the meantime jump over to github and star the repository.
+        https://github.com/zricethezav/gitleaks
+
+      Kudos to Zachary for creating gitleaks. https://github.com/zricethezav
+  EOS
+  end
+        
   def install
     bin.install "gitleaks-darwin-amd64" => "gitleaks"
   end
