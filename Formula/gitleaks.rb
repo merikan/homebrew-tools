6eccf04d0c8b083a634b76ea2cb44f9d03bfc9b1
Peter Merikan <peter@merikan.com>
2018-11-29 18:05:57 +0100

    gitleaks: 1.21.0

--- Formula/gitleaks.rb
+++ Formula/gitleaks.rb
@@ -1,25 +1,25 @@
 class Gitleaks < Formula
   desc "Audit git repos for secrets"
   homepage "https://github.com/zricethezav/gitleaks"
-  url "https://github.com/zricethezav/gitleaks/releases/download/v1.19.3/gitleaks-darwin-amd64"
-  version "1.19.3"
-  sha256 "84396cef693dc064869789a6b56087085639d46beec3025a33bd626533e7534e"
+  url "https://github.com/zricethezav/gitleaks/releases/download/v1.21.0/gitleaks-darwin-amd64"
+  version "1.21.0"
+  sha256 "0a7e9160f8dcb19da61c5068a85b18deb68390b0d7215ff078a57fad29f6a3e9"
 
-  def caveats; <<~EOS
-      This is not the official Formula from the creator of `gitleaks`. It is only for my personal 
-      use until Zachary provides a official Formula.
-      In the meantime jump over to github and star the repository.
-        https://github.com/zricethezav/gitleaks
-
-      Kudos to Zachary for creating gitleaks. https://github.com/zricethezav
-  EOS
-  end
-        
   def install
     bin.install "gitleaks-darwin-amd64" => "gitleaks"
   end
 
+  def caveats; <<~EOS
+    This is not the official Formula from the creator of `gitleaks`. It is only for my personal
+    use until Zachary provides a official Formula.
+    In the meantime jump over to github and star the repository.
+      https://github.com/zricethezav/gitleaks
+
+    Kudos to Zachary for creating gitleaks. https://github.com/zricethezav
+  EOS
+  end
+
   test do
-    system "#{bin}/gitleaks -v"
+    system "#{bin}/gitleaks", "-v"
   end
 end
