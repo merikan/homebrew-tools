57b1d6caf181d0d76b18abec26ccc45ae6dbca7d
Peter Merikan <peter@merikan.com>
2018-12-10 20:33:34 +0100

    gitleaks: 1.22.0

--- Formula/gitleaks.rb
+++ Formula/gitleaks.rb
@@ -1,9 +1,9 @@
 class Gitleaks < Formula
   desc "Audit git repos for secrets"
   homepage "https://github.com/zricethezav/gitleaks"
-  url "https://github.com/zricethezav/gitleaks/releases/download/v1.21.0/gitleaks-darwin-amd64"
-  version "1.21.0"
-  sha256 "0a7e9160f8dcb19da61c5068a85b18deb68390b0d7215ff078a57fad29f6a3e9"
+  url "https://github.com/zricethezav/gitleaks/releases/download/v1.22.0/gitleaks-darwin-amd64"
+  version "1.22.0"
+  sha256 "1bbf4c84e44b53795bbdacc49c5827e29f3188ec51c7886a5bae38a5b79e2331"
 
   def install
     bin.install "gitleaks-darwin-amd64" => "gitleaks"
