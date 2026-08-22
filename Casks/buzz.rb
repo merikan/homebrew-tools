e8d26a5aa2a6f2d6a8e698ae9182c1f0a6bd7798
Peter Merikan <peter@merikan.com>
2026-08-22 18:24:14 +0200

    feat: bump buzz version to v0.5.18

--- Casks/buzz.rb
+++ Casks/buzz.rb
@@ -1,9 +1,9 @@
 cask "buzz" do
   arch arm: "aarch64", intel: "x64"
 
-  version "0.5.14"
-  sha256 arm: "928ab24b32cb921c3a6d252e756c2711d8801b48a8cc998172caf6f70426e1b6",
-         intel: "87ae1a3aaa907d801b79f7e9c6f564f8f594280cba51d6c2a7f1d64d82b7aaf8"
+  version "0.5.18"
+  sha256 arm: "3fc84fabb61dd108ddb66baef3476edeebbe2d10f5acc2ff575c886979df14f9",
+         intel: "21c09a517a3e2c3b7224422ca7fcfca17c4d71321008d9486c1245e0866ce8aa"
 
   url "https://github.com/block/buzz/releases/download/desktop-v#{version}/Buzz_#{version}_#{arch}.dmg"
   name "Buzz"
